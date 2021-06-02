bd697d4d4f0bcfc384a03661a0eb7e2f6d503e61
MaciejBaska <mac.baska@gmail.com>
2021-06-02 11:36:27 +0200

    Add interactivity to the menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Hi! It's nice to see you on my portfolio webside")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+
+}
