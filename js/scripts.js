689078f012cde57759097046896398ecfa93de34
MaciejBaska <mac.baska@gmail.com>
2021-06-08 14:06:41 +0200

    Fix issues after linting

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
-console.log("Hi! It's nice to see you on my portfolio webside")
+console.log("Hi, it's nice to see you on my portfolio site!")
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle' && screen.width > 640) {
-    x.className += ' responsive';
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle' && window.screen.width > 640) {
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
-
 }
